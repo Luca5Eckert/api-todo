5dc04fdad0bd4844953666a9b6704d65a7ec167a
Luca5Eckert <ecklukatv@gmail.com>
2025-08-07 16:57:44 -0300

    feat(user): adiciona atributos basicos da entidade
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

--- src/main/java/com/todoapp/project/modules/user/domain/UserEntity.java
+++ src/main/java/com/todoapp/project/modules/user/domain/UserEntity.java
@@ -1,8 +1,117 @@
 package com.todoapp.project.modules.user.domain;
 
-import jakarta.persistence.Entity;
+import com.todoapp.project.modules.user.domain.enums.TypeUser;
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private final UUID id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private TypeUser type;
+
+    @CreatedDate
+    private final LocalDateTime createAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+
+    @Version
+    private long version;
+
+
+    public UserEntity() {
+        this.id = null;
+        this.name = null;
+        this.email = null;
+        this.password = null;
+        this.type = null;
+        this.createAt = null;
+        this.updateAt = null;
+        this.version = -1;
+    }
+
+    public UserEntity(UUID id, String name, String email, String password, TypeUser type, LocalDateTime createAt, LocalDateTime updateAt, long version) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.type = type;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+        this.version = version;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public TypeUser getType() {
+        return type;
+    }
+
+    public void setType(TypeUser type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
+
+
+
 
 }
