446cb99c0695cb2a53792bbbd34eb906c4e45383
Luca5Eckert <ecklukatv@gmail.com>
2025-08-14 21:11:33 -0300

    feat(create): adiciona repositorio configurado

--- src/main/java/com/todoapp/project/infrastructure/persistence/repositorys/JpaUserRepository.java
+++ src/main/java/com/todoapp/project/infrastructure/persistence/repositorys/JpaUserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface JpaUserRepository extends JpaRepository<UUID, UserEntity>{
+public interface JpaUserRepository extends JpaRepository<UserEntity, UUID>{
 }
--- src/main/java/com/todoapp/project/infrastructure/persistence/repositorys/JpaUserRepositoryAdapter.java
+++ src/main/java/com/todoapp/project/infrastructure/persistence/repositorys/JpaUserRepositoryAdapter.java
@@ -1,8 +1,22 @@
 package com.todoapp.project.infrastructure.persistence.repositorys;
 
+import com.todoapp.project.modules.user.aplication.exception.UserNotFoundByIdException;
+import com.todoapp.project.modules.user.domain.UserEntity;
 import com.todoapp.project.modules.user.domain.port.UserRepository;
+
+import java.util.UUID;
 
 public class JpaUserRepositoryAdapter implements UserRepository {
 
     private JpaUserRepository jpaUserRepository;
+
+    @Override
+    public UserEntity findById(UUID id) {
+        return jpaUserRepository.findById(id).orElseThrow(() -> new UserNotFoundByIdException("User not found by id"));
+    }
+
+    @Override
+    public void save(UserEntity userEntity) {
+        jpaUserRepository.save(userEntity);
+    }
 }
