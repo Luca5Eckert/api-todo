be7e9beb0a4d702ac2b3e12e7efe1baffb3fe96d
Luca5Eckert <ecklukatv@gmail.com>
2025-08-14 21:29:42 -0300

    feat(create): adiciona CreateUserCase no controller

--- src/main/java/com/todoapp/project/modules/user/aplication/controller/UserController.java
+++ src/main/java/com/todoapp/project/modules/user/aplication/controller/UserController.java
@@ -1,7 +1,33 @@
 package com.todoapp.project.modules.user.aplication.controller;
 
+import com.todoapp.project.modules.user.aplication.dto.create.UserCreateRequest;
+import com.todoapp.project.modules.user.aplication.dto.create.UserCreateResponse;
+import com.todoapp.project.modules.user.domain.cases.CreateUserCase;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
+@RequestMapping("/apitodo/user")
 public class UserController {
+
+    private final CreateUserCase createUserCase;
+
+    public UserController(CreateUserCase createUserCase) {
+        this.createUserCase = createUserCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody @Valid UserCreateRequest userCreateRequest){
+        //Gera um id generico para fins de testes
+        UUID id = UUID.randomUUID();
+        UserCreateResponse userCreateResponse = createUserCase.createUser(userCreateRequest, id);
+        return ResponseEntity.ok(userCreateResponse);
+    }
+
 }
