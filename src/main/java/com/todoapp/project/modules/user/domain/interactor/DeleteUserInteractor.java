f9472ad8e08da3056db90fcf8429373a40c3f780
Luca5Eckert <ecklukatv@gmail.com>
2025-08-16 11:35:13 -0300

    feat(delete): adiciona documentação da classe de DeleteUserInteractor

--- src/main/java/com/todoapp/project/modules/user/domain/interactor/DeleteUserInteractor.java
+++ src/main/java/com/todoapp/project/modules/user/domain/interactor/DeleteUserInteractor.java
@@ -8,14 +8,38 @@ import com.todoapp.project.modules.user.domain.port.UserRepository;
 
 import java.util.UUID;
 
+/**
+ * Esse caso de uso é responsavel por deletar os usuários
+ *
+ * <p>Esse interador valida se o usuário que quer executar
+ * tem permissão para deletar outro usuário antes de deletar
+ * o usuário usando o {@link UserRepository}.</p>
+ */
 public class DeleteUserInteractor implements DeleteUserCase {
 
     public final UserRepository userRepository;
 
+    /**
+     * Cria um novo {@code DeleteUserInteractor}
+     *
+     * @param userRepository repository que serve para puxar e deletar usuários
+     */
     public DeleteUserInteractor(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Executa o caso de uso de deletar um usuário
+     *
+     * <p>Primeiro ela verifica se o usuário que está executando tem a
+     * permissão de deletar outros usuários. Se a validação passar, ela
+     * deleta o usuário especificado na requisição.</p>
+     *
+     * @param userDeleteRequest requisição contendo o Id do usuário
+     * @param id O ID do usuário que está executando a ação
+     * @throws UserDeleteValidationException se o usuário não tiver a permissão
+     *
+     */
     @Override
     public void execute(UserDeleteRequest userDeleteRequest, UUID id) {
         UserEntity userExecuter = userRepository.findById(id);
