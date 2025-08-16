5c4ec53c6ef09d78a76fb9c2d435771ecc120b23
Luca5Eckert <ecklukatv@gmail.com>
2025-08-15 21:47:10 -0300

    feat(delete): implementa DeleteUserInteractor

--- src/main/java/com/todoapp/project/modules/user/domain/interactor/DeleteUserInteractor.java
+++ src/main/java/com/todoapp/project/modules/user/domain/interactor/DeleteUserInteractor.java
@@ -1,4 +1,19 @@
 package com.todoapp.project.modules.user.domain.interactor;
 
-public class DeleteUserInteractor {
+import com.todoapp.project.modules.user.aplication.dto.delete.UserDeleteRequest;
+import com.todoapp.project.modules.user.domain.cases.DeleteUserCase;
+import com.todoapp.project.modules.user.domain.port.UserRepository;
+
+public class DeleteUserInteractor implements DeleteUserCase {
+
+    public final UserRepository userRepository;
+
+    public DeleteUserInteractor(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void execute(UserDeleteRequest userDeleteRequest) {
+        userRepository.deleteById(userDeleteRequest.id());
+    }
 }
