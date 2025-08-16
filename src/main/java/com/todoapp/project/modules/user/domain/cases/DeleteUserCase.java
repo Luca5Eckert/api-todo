bd77937471ae2082f48c861654a222a93b678ae9
Luca5Eckert <ecklukatv@gmail.com>
2025-08-15 21:45:14 -0300

    feat(delete): adiciona interface DeleteUserCase

--- src/main/java/com/todoapp/project/modules/user/domain/cases/DeleteUserCase.java
+++ src/main/java/com/todoapp/project/modules/user/domain/cases/DeleteUserCase.java
@@ -1,4 +1,8 @@
 package com.todoapp.project.modules.user.domain.cases;
 
+import com.todoapp.project.modules.user.aplication.dto.delete.UserDeleteRequest;
+
 public interface DeleteUserCase {
+
+    void execute(UserDeleteRequest userDeleteRequest);
 }
