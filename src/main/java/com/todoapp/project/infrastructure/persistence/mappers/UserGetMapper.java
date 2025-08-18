a8c7906a9272fff7ec0c79ca9489bf27278cf327
Luca5Eckert <ecklukatv@gmail.com>
2025-08-18 18:32:16 -0300

    feat(get): adiciona mapper UserGetMapper

---
+++ src/main/java/com/todoapp/project/infrastructure/persistence/mappers/UserGetMapper.java
@@ -0,0 +1,11 @@
+package com.todoapp.project.infrastructure.persistence.mappers;
+
+import com.todoapp.project.modules.user.aplication.dto.get.UserGetResponse;
+import com.todoapp.project.modules.user.domain.UserEntity;
+
+public class UserGetMapper {
+
+    public UserGetResponse toResponse(UserEntity userEntity){
+        return new UserGetResponse(userEntity.getId(), userEntity.getName().getValue(), userEntity.getName().getValue(), userEntity.getType());
+    }
+}
