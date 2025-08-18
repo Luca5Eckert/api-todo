0661f85b4b90c994b7500e096c9212179576ad66
Luca5Eckert <ecklukatv@gmail.com>
2025-08-18 20:46:29 -0300

    feat(edit): adiciona interface para EditUserCase

--- src/main/java/com/todoapp/project/modules/user/domain/cases/EditUserCase.java
+++ src/main/java/com/todoapp/project/modules/user/domain/cases/EditUserCase.java
@@ -1,5 +1,8 @@
 package com.todoapp.project.modules.user.domain.cases;
 
+import com.todoapp.project.modules.user.aplication.dto.edit.UserEditRequest;
+import com.todoapp.project.modules.user.aplication.dto.edit.UserEditResponse;
+
 public interface EditUserCase {
 
     UserEditResponse execute(UserEditRequest editRequest);
