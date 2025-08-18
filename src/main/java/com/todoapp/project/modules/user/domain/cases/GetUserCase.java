d5d22e9a73c6ce0901eaf993783bcd4f6955a19b
Luca5Eckert <ecklukatv@gmail.com>
2025-08-18 18:28:59 -0300

    feat(get): adiciona interface do GetUserCase

--- src/main/java/com/todoapp/project/modules/user/domain/cases/GetUserCase.java
+++ src/main/java/com/todoapp/project/modules/user/domain/cases/GetUserCase.java
@@ -1,6 +1,9 @@
 package com.todoapp.project.modules.user.domain.cases;
 
+import com.todoapp.project.modules.user.aplication.dto.get.UserGetRequest;
+import com.todoapp.project.modules.user.aplication.dto.get.UserGetResponse;
+
 public interface GetUserCase {
 
-
+    UserGetResponse execute(UserGetRequest userGetRequest);
 }
