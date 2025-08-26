2c17a98d6319d6ee231cf9959237cb97e2fc2bcf
LucasEckert <ecklukatv@gmail.com>
2025-08-26 19:08:03 -0300

    #15 feat: adiciona conexão com o banco de dados (#19)
    * #15 feat: adiciona classe para converter email para banco de dados
    
    * #15 feat: adiciona configurações de banco de dados no application.properties
    
    * #15 fix: adiciona dependencia do driver sql-connection
    
    * #15 feat: adiciona classe para converter name para banco de dados
    
    * #15 fix: arruma dependencias no pom.xml

--- src/main/java/com/todoapp/project/modules/user/domain/UserEntity.java
+++ src/main/java/com/todoapp/project/modules/user/domain/UserEntity.java
@@ -1,5 +1,7 @@
 package com.todoapp.project.modules.user.domain;
 
+import com.todoapp.project.infrastructure.persistence.user.converter.EmailConverter;
+import com.todoapp.project.infrastructure.persistence.user.converter.NameConverter;
 import com.todoapp.project.modules.user.domain.enums.TypeUser;
 import com.todoapp.project.modules.user.domain.valueobjects.Email;
 import com.todoapp.project.modules.user.domain.valueobjects.Name;
@@ -18,8 +20,10 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     private final UUID id;
 
+    @Convert(converter = NameConverter.class)
     private Name name;
 
+    @Convert(converter = EmailConverter.class)
     private Email email;
 
     private String password;
