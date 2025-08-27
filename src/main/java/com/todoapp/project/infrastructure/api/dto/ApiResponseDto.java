42f885eebb1d6ec43f3f1fb9ff448179bc8ae0a7
Luca5Eckert <ecklukatv@gmail.com>
2025-08-27 14:01:13 -0300

    feat: adiciona métodos estasticos para construir a resposta de API

---
+++ src/main/java/com/todoapp/project/infrastructure/api/dto/ApiResponseDto.java
@@ -0,0 +1,18 @@
+package com.todoapp.project.infrastructure.api.dto;
+
+import java.time.LocalDateTime;
+
+public record ApiResponseDto<T>(int typeResponse, String errorCode, String message, T data, LocalDateTime dataTime) {
+
+    public static ApiResponseDto<Void> error(int typeResponse, String errorCode, String message){
+        return new ApiResponseDto<Void>(typeResponse, errorCode, message, null, LocalDateTime.now());
+    }
+
+    public static <T> ApiResponseDto<T> sucess(int typeResponse, String message, T data){
+        return new ApiResponseDto<T>(typeResponse, null, message, data, LocalDateTime.now());
+    }
+
+    public static ApiResponseDto<Void> sucess(int typeResponse, String message){
+        return new ApiResponseDto<Void>(typeResponse, null, message, null, LocalDateTime.now());
+    }
+}
