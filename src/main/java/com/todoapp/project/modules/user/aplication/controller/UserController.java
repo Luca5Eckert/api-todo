6d0be951eb2e2ee05b63c75ca29fab6f6b636d6f
Luca5Eckert <ecklukatv@gmail.com>
2025-08-27 14:18:30 -0300

    feat: adiciona padronização de resposta em UserController

--- src/main/java/com/todoapp/project/modules/user/aplication/controller/UserController.java
+++ src/main/java/com/todoapp/project/modules/user/aplication/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.todoapp.project.modules.user.aplication.controller;
 
+import com.todoapp.project.infrastructure.api.dto.ApiResponseDto;
 import com.todoapp.project.modules.user.aplication.dto.create.UserCreateRequest;
 import com.todoapp.project.modules.user.aplication.dto.create.UserCreateResponse;
 import com.todoapp.project.modules.user.aplication.dto.delete.UserDeleteRequest;
@@ -10,6 +11,7 @@ import com.todoapp.project.modules.user.aplication.dto.get.UserGetResponse;
 import com.todoapp.project.modules.user.aplication.port.UserService;
 import com.todoapp.project.modules.user.domain.cases.CreateUserCase;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
@@ -27,35 +29,43 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<?> createUser(@RequestBody @Valid UserCreateRequest userCreateRequest){
+    public ResponseEntity<ApiResponseDto<UserCreateResponse>> createUser(@RequestBody @Valid UserCreateRequest userCreateRequest){
         //Gera um id generico para fins de testes
+
         UUID id = UUID.randomUUID();
         UserCreateResponse userCreateResponse = userService.createUser(userCreateRequest, id);
-        return ResponseEntity.ok(userCreateResponse);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponseDto.sucess(HttpStatus.CREATED.value(), "User created with success", userCreateResponse));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UserEditResponse>  editUser(@RequestBody @Valid UserEditRequest userEditRequest, @PathVariable UUID id){
+    public ResponseEntity<ApiResponseDto<UserEditResponse>>  editUser(@RequestBody @Valid UserEditRequest userEditRequest, @PathVariable UUID id){
         //Gera um id generico para fins de testes
+
         UUID idExecuter = UUID.randomUUID();
-        UserEditResponse userCreateResponse = userService.editResponse(userEditRequest, idExecuter, id);
-        return ResponseEntity.ok(userCreateResponse);
+        UserEditResponse userEditResponse = userService.editResponse(userEditRequest, idExecuter, id);
+
+        return ResponseEntity.ok(ApiResponseDto.sucess(200, "User edited with sucess", userEditResponse));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable UUID id){
+    public ResponseEntity<ApiResponseDto<Void>> deleteUser(@PathVariable UUID id){
         //Gera um id generico para fins de testes
         UUID idExecuter = UUID.randomUUID();
+
         UserDeleteRequest userDeleteRequest = new UserDeleteRequest(id);
         userService.deleteUser(userDeleteRequest, id);
-        return ResponseEntity.ok("User deleted");
+
+        return ResponseEntity.ok(ApiResponseDto.sucess(200, "User deleted with sucess"));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getUser(@PathVariable UUID id){
+    public ResponseEntity<ApiResponseDto<UserGetResponse>> getUser(@PathVariable UUID id){
+
         UserGetRequest userGetRequest = new UserGetRequest(id);
         UserGetResponse userGetResponse = userService.getUser(userGetRequest);
-        return ResponseEntity.ok(userGetResponse);
+
+        return ResponseEntity.ok(ApiResponseDto.sucess(200, "User found with sucess", userGetResponse));
     }
 
 
