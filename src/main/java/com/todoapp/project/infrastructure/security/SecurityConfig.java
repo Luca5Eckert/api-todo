946ea7b0730068c3a9d8800f48361181c2b467c7
Luca5Eckert <ecklukatv@gmail.com>
2025-09-02 21:49:09 -0300

    #28 fix: altera configuração de rota users

--- src/main/java/com/todoapp/project/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/todoapp/project/infrastructure/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/apitodo/user/**").authenticated();
+                    auth.requestMatchers("/apitodo/users/**").authenticated();
                     auth.anyRequest().permitAll();
                 })
                 .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
